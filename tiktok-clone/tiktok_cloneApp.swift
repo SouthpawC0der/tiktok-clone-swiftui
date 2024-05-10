8e5e63bb97b16e13547c7f33904c677725a9fe93
southpawcoder <southpaw1224@outlook.com>
2024-05-09 23:21:22 -0400

    Initial Commit

---
+++ tiktok-clone/tiktok_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktok_cloneApp.swift
+//  tiktok-clone
+//
+//  Created by Christopher Doyle on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct tiktok_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
